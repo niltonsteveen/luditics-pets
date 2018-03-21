98bf64bce3f5e1222f2cf2d7ea3f4603eb4b6c9f
jonathan <jtorresv232@gmail.com>
2018-03-20 20:48:57 -0500

    modelo y serializers

--- apiRest_pets/serializers.py
+++ apiRest_pets/serializers.py
@@ -1,76 +1,81 @@
 from rest_framework import serializers
 from .models import Alumno
 from .models import Sesion
-from .models import Desempeno
-from .models import Historias
-from .models import SabiasQue
+from .models import Estadistica
+from .models import Texto
+from .models import Tipo_Texto
 from .models import Permisos
-from .models import Actividad
 from .models import Grupo
 from .models import Docente
 from .models import Grupo_Docente
+from .models import Juego
+from .models import Configuracion
+
+
+""" /////////////////DONE/////////////////////"""
 
 class AlumnoSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model=Alumno
-		fields=('id','nombreCompleto','grupo')
-
+		fields=('id','nombre_completo','grupo')
 
 class SesionSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model=Sesion
-		fields=('idSesion', 'fechaInicio', 'idAlumno')
-
-
-class DesempenoSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=Desempeno
-		fields=('id', 'numeroAciertos', 'numeroFallos','nivel', 'tipoOperacion', 'idAlumno', 'idSesion', 'fechaReporte', 'grupo')
-
-
-class HistoriasSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=Historias
-		fields=('id', 'texto', 'tipoMascota', 'habilitado','tipoOperacion', 'nivel')
-
-
-class SabiasQueSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=SabiasQue
-		fields=('id', 'texto', 'habilitado')
-
-class PermisosSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=Permisos
-		fields=('id', 'modulo', 'descripcion')
-
-class ActividadSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=Actividad
-		fields=('id', 'texto', 'habilitado')
+		fields=('id', 'fechaInicio', 'idAlumno','juego')
 
 class GrupoSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model=Grupo
-		fields=('id', 'grupoId', 'descripcion')
-
-
-class DocenteSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model=Docente
-		fields=('identificacion', 'nombre', 'password','permisos', 'correo', 'permisos')
+		fields=('id', 'descripcion')
 
 class Grupo_DocenteSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model=Grupo_Docente
 		fields=('docente', 'grupo')
+
+class DocenteSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Docente
+		fields=('id', 'nombre', 'contrasena', 'correo')
+
+class TextoSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Texto
+		fields=('id', 'tipoTexto', 'habilitado', 'descripcion')
+
+class JuegoSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Juego
+		fields=('id', 'descripcion', 'configuracion')
+
+class ConfiguracionSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Configuracion
+		fields=('id', 'rangoMin', 'rangoMax', 'orden', 'ayuda', 'numPorOp')
+
+class Tipo_TextoSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Tipo_Texto
+		fields=('id', 'nombre')
+
+class EstadisticaSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Estadistica
+		fields=('id','numeroAciertos','numeroFallos', 'nivel', 'tipoOperacion','fechaReporte','sesion')
+
+class PermisosSerializer(serializers.ModelSerializer):
+
+	class Meta:
+		model=Permisos
+		fields=('id', 'modulo', 'descripcion')
